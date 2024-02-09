314a2ee0abdc0c6f6f309d830a2c099c3b37b131
Melker Ulander <melker.ulander@pm.me>
2024-02-09 16:03:50 +0100

    Add Home Assistant
    Also, move docker-compose.yml to its own file

--- setup.sh
+++ setup.sh
@@ -9,31 +9,6 @@ fi
 echo "Installing docker-compose"
 sudo pacman --needed -S docker docker-compose
 
-mkdir -p ~/pihole
-cd ~/pihole
-
-echo "
-version: '3'
-
-# More info at https://github.com/pi-hole/docker-pi-hole/
-services:
-  pihole:
-    container_name: pihole
-    image: pihole/pihole:latest
-    ports:
-      - 53:53/tcp
-      - 53:53/udp
-      - 80:80/tcp
-    environment:
-      TZ: Europe/Stockholm
-      WEBPASSWORD: $PASSWORD
-    # Volumes store your data between container upgrades
-    volumes:
-      - ./etc-pihole:/etc/pihole
-      - ./etc-dnsmasq.d:/etc/dnsmasq.d
-    restart: unless-stopped
-" > docker-compose.yml
-
-echo "Running Pi-Hole container"
+echo "Spinning up containers defined in docker-compose.yml"
 sudo systemctl start docker
 sudo docker-compose up -d
